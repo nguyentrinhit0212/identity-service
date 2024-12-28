e2f37062f2b4996df2ad4d1063c88e0c9e8eac95
NguyenTH <141311098+nguyentrinhit0212@users.noreply.github.com>
2024-12-28 23:43:09 +0700

    fix cors

--- cmd/main.go
+++ cmd/main.go
@@ -37,7 +37,7 @@ func main() {
 	// Setup router with middleware
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
